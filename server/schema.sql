f2290930c9bc07ac460f962caf01f9426a3f4675
khengschew <khengc@gmail.com>
2019-01-25 15:37:31 -0800

    Add schema for messages database

--- server/schema.sql
+++ server/schema.sql
@@ -1,9 +1,37 @@
+DROP DATABASE IF EXISTS chat;
+
 CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE user (
+  id INT AUTO_INCREMENT,
+  name VARCHAR(50),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE room (
+  id INT AUTO_INCREMENT,
+  name VARCHAR(100),
+  PRIMARY KEY (id)
+);
+
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  id INT AUTO_INCREMENT,
+  userId INT,
+  roomId INT,
+  text text,
+  createdAt timestamp NULL,
+  updatedAt timestamp NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (userId)
+    REFERENCES user(id)
+    ON DELETE CASCADE,
+  FOREIGN KEY (roomId)
+    REFERENCES room(id)
+    ON DELETE CASCADE,
+  INDEX (userId),
+  INDEX (roomId)
 );
 
 /* Create other tables and define schemas for them here! */
